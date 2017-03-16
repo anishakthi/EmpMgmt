3a8b2af2c69999c24a76b0077de0467813fc28c8
anitha <anishakthi@gmail.com>
2017-03-16 20:20:54 +0530

    Final commit

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,23 +1,55 @@
 class EmployeesController < ApplicationController
-  
-  def new 
-  end
-  
+
+  # Method to List all Employees
   def index
   	@employee = Employee.all
-  	end
-    
-  def create
-    @employee = Employee.new(add_employee_request)
-    # returns boolean 
-    @employee.save
-    render :action => 'show'
   end
   
+  # Method to render new Page
+  def new 
+    @employee = Employee.new
+  end
+  
+  # Method to create Employee.
+  def create
+      @employee = Employee.new(add_employee_request)
+      # returns boolean
+      if @employee.save
+          redirect_to @employee
+        else
+          # Renders the new page with the errors
+          render 'new'
+       end
+  end
+  
+  #Method to show the Employee details of the id passed.
   def show
   		@employee = Employee.find(params[:id]) 
   end
-    
+  
+  # Method to populate the edit page with the values.
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+  
+  # Methos to update the Employee details
+  def update
+    @employee = Employee.find(params[:id])
+    if @employee.update(add_employee_request)
+      redirect_to @employee
+    else
+      render 'edit'
+    end
+  end
+  
+  #Method to delete the Employee document.
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+    redirect_to employees_path
+  end
+  
+  # Method to tell which params are allowed inside the controller action methods.
   private 
   def add_employee_request
   		params.require(:employee).permit(:name, :doj, :status, :dob)
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ApplicationRecord
-
+  validates :name, presence: true, length: {maximum: 50} 
+  validates :status, acceptance: {accept: ['Probation','Confirmed']}
+  validates :dob, timeliness: { on_or_before: lambda { Date.current }, type: :date }
+  validates :doj, presence: true
 end
